818d457c98eb019b4dcf7d6e631c332d1683b01f
Andrey Strebkov <as19772023as@gmail.com>
2023-12-14 23:05:53 +0300

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Graph<T> {
+    private List<Vertex<T>> vertices = new ArrayList<>();
+
+    public Vertex<T> createVertex(T value) {
+        Vertex<T> v = new Vertex<>(value);
+        vertices.add(v);
+        return v;
+    }
+
+    public void createEdge(Vertex<T> a, Vertex<T> b) {
+        // добавляем их друг друга в их списки смежности
+        // ВАШ КОД
+        a.getAdjacent().add(b);
+
+    }
+
+    public boolean isConnected(Vertex<T> a, Vertex<T> b) {
+        return dfsFind(a, b, new HashSet<>()); // рекурсивный обход в глубину
+    }
+
+    // метод отвечает на вопрос, нашли ли мы обходом из v вершину target с учётом
+    // посещённых вершин, которые записаны в visited
+    private boolean dfsFind(Vertex<T> v, Vertex<T> target, Set<Vertex<T>> visited) {
+        // если вершина в которую зашли (v) это та которую мы искали (target), то поиск закончен
+        if (v.equals(target)) {
+            return true; // нашли
+        }
+        visited.add(v); // запоминаем вершину которую посетили
+
+        // ВАШ КОД
+        // перебираем все смежные вершины у v
+        // если такую вершину ещё не посещали, заходим рекурсивно в неё
+        // если такой заход завершился нахождением target-а - выходим из метода с true
+
+
+        for (Vertex next : v.getAdjacent()) {
+            if (next.equals(target)) {
+                return true; // нашли
+            }
+
+            if (!visited.contains(next)) {
+               if(dfsFind(next, target, visited)) return true;
+            }
+        }
+        return false; // ничего не нашли
+    }
+}
